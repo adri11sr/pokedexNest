c6fc262f87ce9a83f44d587fe30f767ee7d501a3
adri11sr <adrysanchezr@gmail.com>
2025-10-30 13:36:24 +0100

    Se implemneta el update de pokemon con el manejo de excepciones

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -24,13 +24,7 @@ export class PokemonService {
       return pokemon;
       
     } catch (error) {
-      
-      if (error.code === 11000){
-        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
-      }
-
-      console.log(error);
-      throw new InternalServerErrorException(`Can't create Pokemon - Cjeck server logs`);
+      this.handleExceptions(error);
     }
   }
 
@@ -61,11 +55,32 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.findOne(term);
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+
+    try {
+      await pokemon.updateOne(updatePokemonDto, {new: true})
+      return {...pokemon.toJSON(), ...updatePokemonDto};
+      
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+
+
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions (error:any){
+    if (error.code === 11000){
+        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
+      }
+
+      throw new InternalServerErrorException(`Can't update Pokemon - Check server logs`);
   }
 }
