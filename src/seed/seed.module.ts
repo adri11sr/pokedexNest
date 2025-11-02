959708276b5b286badfb59aac45df295dc12ec8f
adri11sr <adrysanchezr@gmail.com>
2025-11-02 20:20:40 +0100

    Creaación de la semilla

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports:[PokemonModule,
+    PokemonModule
+  ]
 })
 export class SeedModule {}
